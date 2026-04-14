13e8bd48026f1da913fc637d6f3a496b55e6ca57
Larissadods <larissa.pereira.araujo@escola.pr.gov.br>
2026-04-14 14:32:00 -0300

    Add files via upload

---
+++ Admin.js
@@ -0,0 +1,15 @@
+import User from "./User.js";
+
+class Admin extends User {
+    constructor(nome, email, nascimento, role = 'admin', ativo = true) {
+        super(nome, email, nascimento, role, ativo = true)
+    }
+
+    criarCurso(nomeDoCurso, vagas) {
+        return `Curso de ${nomeDoCurso} criado com ${vagas} vagas`
+    }
+
+}
+
+const novoAdmin = new Admin('Rofrigo', 'r@r.com', '2021-01-01')
+console.log(novoAdmin.criarCurso('JS', 20))
